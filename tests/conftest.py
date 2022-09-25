d9a42b0ddc4b8206d0d357e23ad6494cca3620a7
Kim Nes Leirvik <kim.leirvik@niva.no>
2022-09-25 09:57:33 +0200

    add enviorment defaults for tests

---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+import pytest
+
+import os
+
+def pytest_generate_tests(metafunc):
+    os.environ["DB_HOST"] = "dbhost"
+    os.environ["DB_USER"] = "dbuser"
+    os.environ["THREDDS_SERVER"] = "thredds"
