c06ce239b94e6fede327c3d1c8c96544b83b4b51
Kim Nes Leirvik <kim.leirvik@niva.no>
2022-09-28 08:56:17 +0200

    terst time stamp

---
+++ tests/test_utils.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+import imp
+import numpy as np
+
+from dataexport import utils
+
+def test_numpy_to_datetime():
+    assert utils.numpy_to_datetime(np.datetime64('2019-08-26T00:00:00.00', 's')) == datetime(2019,8,26)
+    assert utils.numpy_to_datetime(np.datetime64('2019-08-26T00:00:00.00', 'us')) == datetime(2019,8,26)
+    assert utils.numpy_to_datetime(np.datetime64('2019-08-26T00:00:00.00', 'ns')) == datetime(2019,8,26)
