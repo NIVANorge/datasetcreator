867af3495ff25f5c401a5bb70f9e6aa8bdfd6acf
Kim Nes Leirvik <kim.leirvik@niva.no>
2022-10-01 17:25:12 +0200

    add datetime code

--- dataexport/utils.py
+++ dataexport/utils.py
@@ -1,5 +1,7 @@
+from dataclasses import dataclass
 import numpy as np
-from datetime import datetime
+from typing import List
+from datetime import datetime, timedelta
 
 
 def numpy_to_datetime(dt: np.datetime64) -> datetime:
@@ -12,3 +14,20 @@ def numpy_to_datetime(dt: np.datetime64) -> datetime:
         case "datetime64[ns]":
             factor = 1e9
     return datetime.utcfromtimestamp(dt.astype(int) / factor)
+
+
+@dataclass
+class DatetimeInterval:
+    start_time: datetime
+    end_time: datetime
+
+
+def datetime_intervals(start_time: datetime, end_time: datetime, delta: timedelta) -> List[DatetimeInterval]:
+    """Generate datetime intervals of size delta"""
+
+    intervals = []
+    current = start_time
+    while current < end_time:
+        intervals.append(DatetimeInterval(current, current + delta))
+        current = intervals[-1].end_time
+    return intervals
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 import numpy as np
+import pytest
+from datetime import datetime, timedelta
 
 from dataexport import utils
 
@@ -8,3 +10,33 @@ def test_numpy_to_datetime():
     assert utils.numpy_to_datetime(np.datetime64("2019-08-26T00:00:00.00", "s")) == datetime(2019, 8, 26)
     assert utils.numpy_to_datetime(np.datetime64("2019-08-26T00:00:00.00", "us")) == datetime(2019, 8, 26)
     assert utils.numpy_to_datetime(np.datetime64("2019-08-26T00:00:00.00", "ns")) == datetime(2019, 8, 26)
+
+
+@pytest.mark.parametrize(
+    "start_time,end_time, delta, expected",
+    [
+        (
+            datetime(2022, 9, 29, 9, 45),
+            datetime(2022, 9, 30, 9, 45),
+            timedelta(days=1),
+            1,
+        ),
+        (
+            datetime(2022, 9, 20, 9, 45),
+            datetime(2022, 9, 30, 9, 45),
+            timedelta(days=1),
+            10,
+        ),
+        (
+            datetime(2022, 9, 30, 9, 45),
+            datetime(2022, 9, 30, 10, 45),
+            timedelta(minutes=30),
+            2,
+        ),
+    ],
+)
+def test_time_interval(start_time, end_time, delta, expected):
+
+    intervals = utils.datetime_intervals(start_time, end_time, delta)
+    assert len(intervals) == expected
+    assert intervals[-1].start_time + delta == intervals[-1].end_time
