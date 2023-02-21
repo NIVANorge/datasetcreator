929debce8b208cadf0d14a43129aada084e12b29
ZofiaRudjord <zofia.rudjord@niva.no>
2023-02-21 11:54:00 +0100

    Add filtering

--- dscreator/sources/ferrybox/extractor.py
+++ dscreator/sources/ferrybox/extractor.py
@@ -16,9 +16,9 @@ class TrajectoryExtractor(BaseExtractor):
     variable_codes: List[str]
 
     def fetch_slice(
-        self,
-        start_time: datetime,
-        end_time: datetime,
+            self,
+            start_time: datetime,
+            end_time: datetime,
     ) -> NamedTrajectory:
         """Create a Timeseries from tsb
         The timeseries is limited to start_time<t<=end_time.
@@ -27,7 +27,7 @@ class TrajectoryExtractor(BaseExtractor):
 
         named_timearrays = []
         track = get_track(self.engine, MAPPER[self.platform_code]["track"], start_time, end_time)
-        track_datetime = track.datetime
+        track_datetime = list(track.datetime)
         for vcode in self.variable_codes:
             res = query_ts(uuid=MAPPER[self.platform_code][vcode])
             if len(res.values) > 0:
@@ -37,9 +37,16 @@ class TrajectoryExtractor(BaseExtractor):
             else:
                 logging.info(f"No values for {vcode} for time period ({start_time} : {end_time})")
                 named_timearrays.append(NamedTimeArray(vcode, [None for x in range(len(track_datetime))]))
-        # values = [ts.values for ts in named_timearrays]
-        track_values = track.values
-
+        # Get indices of all None values
+        i_None = [[i for i, v in enumerate(ts.values) if v is None] for ts in named_timearrays]
+        # If None appears for all measurements, it means there is no valid data
+        i_noData = list(
+            set([index for index in i_None[0] for j in range(1, len(named_timearrays)) if index in i_None[j]]))
+        named_timearrays = [
+            NamedTimeArray(nta.variable_name, [v for i, v in enumerate(nta.values) if i not in i_noData]) for nta in
+            named_timearrays]
+        track_values = [tv for i, tv in enumerate(track.values) if i not in i_noData]
+        track_datetime = [tdt for i, tdt in enumerate(track_datetime) if i not in i_noData]
         return NamedTrajectory(array_list=named_timearrays, datetime_list=track_datetime, locations=track_values)
 
     def _timestamp(self, is_asc: bool) -> datetime:
