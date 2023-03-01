70008beb4982ed120f1a3d926bdca881872c1a40
Kim Nes Leirvik <kim.leirvik@niva.no>
2023-03-01 14:50:52 +0100

    add splitter on trajectory

--- scripts/trajectory_plot.py
+++ scripts/trajectory_plot.py
@@ -1,15 +1,14 @@
 # Example plot with moving pandas
 # Install extra dependecies with:
-# `poetry install --with plotting
+# `poetry install
 
 #%%
 import xarray as xr
 import movingpandas as mpd
 import holoviews as hv
+from datetime import timedelta
+import matplotlib.pyplot as plt
 
-# setting bokeh as backend
-hv.extension('bokeh')
-from bokeh.plotting import show
 # %% Or replace with you path to example trajectory
 ds = xr.open_dataset("../catalog/datasets/norsoop/rt_ferrybox_FA/2022-12-12T160100Z_acdd_rt_ferrybox_fa.nc")
 #%%
@@ -19,9 +18,15 @@ traj = mpd.Trajectory(
 #%%
 traj.df.head()
 #%%
-plot = traj.hvplot(c='salinity', title=ds.attrs["title"], line_width=2, frame_width=700, frame_height=500)
-
+# This can be a bit slow
+traj.hvplot(title=ds.attrs["title"], line_width=2, frame_width=700, frame_height=500)
 # %%
-# this can be a bit slow
-show(hv.render(plot))
+split = mpd.ObservationGapSplitter(traj).split(gap=timedelta(minutes=60))
+split
 # %%
+split.to_traj_gdf()
+# %%
+#assumes you have a 5 sub trajectories
+fig, axes = plt.subplots(nrows=1, ncols=5)
+for i, traj in enumerate(split.trajectories[0:5]):
+    traj.plot(ax=axes[i], linewidth=5.0, capstyle="round", column="salinity", vmax=20)
