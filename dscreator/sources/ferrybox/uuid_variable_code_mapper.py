f95550bfc5915879a2fdb1267277717cb430be85
Kim Nes Leirvik <kim.leirvik@niva.no>
2023-10-17 17:23:50 +0200

    update uuid to match paths on msferrybox

--- dscreator/sources/ferrybox/uuid_variable_code_mapper.py
+++ dscreator/sources/ferrybox/uuid_variable_code_mapper.py
@@ -4,7 +4,7 @@ MAPPER = {
     {
         "Temperature": "720b78cb-3e82-4c4d-9b63-7d1ae1b7afc1",  # degC, FA/INLET/SBE38/TEMPERATURE/RAW
         "Salinity": "314cd400-14a7-489a-ab97-bce6b11ad068",  # PSU, SBE45, FA/FERRYBOX/SBE45/SALINITY/RAW
-        "Oxygen": "b345a87f-bdb7-4115-8290-710a1b5264f5",  # RBR, %, FA/ferrybox/INLET/OPTODE/OXYGEN/SATURATION
+        "Oxygen": "e03fba93-1fed-49c2-ac5a-601dc2475915",  # RBR, %, FA/ferrybox/INLET/OPTODE/OXYGEN/SATURATION
         "track": "4d9ff393-25a3-47b8-aaf1-8fbbccfec3c3",  # FA/gpstrack
     },
     "FA_20":
@@ -12,7 +12,7 @@ MAPPER = {
     {
         "Temperature": "720b78cb-3e82-4c4d-9b63-7d1ae1b7afc1",  # degC, FA/INLET/SBE38/TEMPERATURE/RAW
         "Salinity": "314cd400-14a7-489a-ab97-bce6b11ad068",  # PSU, SBE45, FA/FERRYBOX/SBE45/SALINITY/RAW
-        "Oxygen": "b345a87f-bdb7-4115-8290-710a1b5264f5",  # RBR, %, FA/FERRYBOX/OPTODE/OXYGEN_SATURATION/RAW
+        "Oxygen": "b345a87f-bdb7-4115-8290-710a1b5264f5",  # RBR, %, FA/FERRYBOX/RBR/OXYGEN_SATURATION/RAW
         "track": "4d9ff393-25a3-47b8-aaf1-8fbbccfec3c3",  # FA/gpstrack
     },
     "TF": {},
