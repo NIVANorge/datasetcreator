f525c3c5fe1163666fe1c69adff6312b99ad3b45
Kim Nes Leirvik <kim.leirvik@niva.no>
2023-10-18 14:28:29 +0200

    update records

--- scripts/fetch_metadata.py
+++ scripts/fetch_metadata.py
@@ -5,12 +5,12 @@ current_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "
 tds_url = "https://thredds.niva.no/thredds/iso/datasets"
 cat_base = "catalog=file:/usr/local/tomcat/content/thredds/subcatalogs"
 datasets = [
-    ("msource-inlet", "d2675936-8ebf-4fc5-988c-4a5198b2df57", "loggers"),
-    ("msource-outlet", "4b123377-e0a6-4c7e-b466-2f8a3199bc86", "loggers")
+    ("loggers/msource-inlet", "d2675936-8ebf-4fc5-988c-4a5198b2df57", "loggers"),
+    ("loggers/msource-outlet", "4b123377-e0a6-4c7e-b466-2f8a3199bc86", "loggers")
 ]
 
 for ds_name, uuid, cat_name in datasets:
     ds_url = f"{tds_url}/{ds_name}.nc?{cat_base}/{cat_name}.xml&dataset={uuid}"
     res = requests.get(ds_url)
-    with open(os.path.join(current_path, f"{ds_name}.xml"), "w") as f:
+    with open(os.path.join(current_path, f"{ds_name.split('/')[-1]}.xml"), "w") as f:
         f.write(res.text)
