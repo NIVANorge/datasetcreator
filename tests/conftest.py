efe38f6126e5a2d81b09f824626af6f3a1b50143
Kim Leirvik <kim.leirvik@niva.no>
2025-01-14 08:02:55 +0100

    db wait condition for tests
    Add conftest wait to avoid tests running before db is ready, close #37

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,12 @@ import os
 import pytest
 from sqlalchemy import create_engine
 
+def db_is_ready(url):
+    try:
+        create_engine(url).connect()
+        return True
+    except Exception:
+        return False
 
 def pytest_generate_tests(metafunc):
     os.environ["DATABASE_URL"] = "postgresql://postgres:postgres@127.0.0.1:32998/db"
@@ -12,7 +18,12 @@ def pytest_generate_tests(metafunc):
 
 @pytest.fixture(scope="session")
 def db_engine(docker_ip, docker_services):
+
     port = docker_services.port_for("test-db", 5432)
     host = docker_ip
+    url = f"postgresql://postgres:postgres@{host}:{port}/db"
+    docker_services.wait_until_responsive(
+        timeout=30.0, pause=0.1, check=lambda: db_is_ready(url)
+    )
 
-    return create_engine(f"postgresql://postgres:postgres@{host}:{port}/db")
+    return create_engine(url)
